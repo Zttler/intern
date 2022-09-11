78d199fa11f747f3e2f0c68284e11557fe404430
ZATTLER <51602131+u6288144@users.noreply.github.com>
2022-09-11 18:58:03 +0700

    Add files via upload

---
+++ New folder/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Vulnerable is ............</title>
+    <link rel="stylesheet" href="css/materialize.min.css">
+    <script src="js/materialize.min.js">
+
+    </script>
+  </head>
+  <body>
+<div class="container">
+
+
+
+<!-- Navbar goes here -->
+
+   <!-- Page Layout here -->
+   <div class="row">
+
+     <div class="col s3">
+       <?php include("nav.php"); ?>
+     </div>
+
+     <div class="col s9">
+       <h3>Hello</h3>
+
+   </div>
+
+<?php include("footer.php"); ?>
+</div>
+  </body>
+</html>
---
+++ New folder/nav.php
@@ -0,0 +1,4 @@
+<br><div class="collection">
+        <a href="index.php" class="collection-item">Home</a>
+        <a href="dir/challenge.php" class="collection-item">Try me </a>
+</div>
